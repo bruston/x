84413e3006ba9b233d1464924b6817a44f843bf2
Benjamin Ruston <benjy.ruston@gmail.com>
2015-03-04 12:55:21 +0000

    Specify cipher suites

--- strictertls/main.go
+++ strictertls/main.go
@@ -8,13 +8,12 @@ import (
 )
 
 func main() {
-	cert := flag.String("cert", "", "A PEM encoded certificate file")
-	key := flag.String("key", "", "A PEM encoded private key file")
+	cert := flag.String("cert", "", "Certificate file")
+	key := flag.String("key", "", "Private key file")
 	listen := flag.String("listen", ":8090", "The host and port to listen on")
 	flag.Parse()
 
-	// Support only TLS1.0, TLS1.1 and TLS1.2
-	cfg := &tls.Config{MinVersion: tls.VersionTLS10}
+	cfg := newRestrictedCfg()
 	server := &http.Server{Addr: *listen, TLSConfig: cfg, Handler: http.DefaultServeMux}
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
@@ -25,5 +24,21 @@ func main() {
 
 	if err := server.ListenAndServeTLS(*cert, *key); err != nil {
 		log.Fatal(err)
+	}
+}
+
+func newRestrictedCfg() *tls.Config {
+	return &tls.Config{
+		MinVersion:             tls.VersionTLS12,
+		SessionTicketsDisabled: true,
+		CipherSuites: []uint16{tls.TLS_RSA_WITH_AES_128_CBC_SHA,
+			tls.TLS_RSA_WITH_AES_256_CBC_SHA,
+			tls.TLS_ECDHE_ECDSA_WITH_AES_128_CBC_SHA,
+			tls.TLS_ECDHE_ECDSA_WITH_AES_256_CBC_SHA,
+			tls.TLS_ECDHE_RSA_WITH_AES_128_CBC_SHA,
+			tls.TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA,
+			tls.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
+			tls.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,
+		},
 	}
 }
