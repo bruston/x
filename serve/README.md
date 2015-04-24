beeb2ee6ba19580a61482d09e92f18bf26ed8b23
Benjamin Ruston <benjy.ruston@gmail.com>
2015-04-24 18:21:23 +0100

    init serve

---
+++ serve/README.md
@@ -0,0 +1,12 @@
+serve
+=====
+
+Quickly start a webserver serving from a directory.
+
+## Usage
+<pre>
+Usage of ./serve:
+-debug=true: Log request information to the console.
+-dir="": The directory to serve. Current directory used if left empty.
+-listen="127.0.0.1:8000": The host:port to listen on.
+</pre>
---
+++ serve/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"flag"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/bruston/handlers/debug"
+)
+
+func main() {
+	listen := flag.String("listen", "127.0.0.1:8000", "The host:port to listen on.")
+	dir := flag.String("dir", "", "The directory to serve. Current directory used if left empty.")
+	showDebug := flag.Bool("debug", true, "Log request information to the console.")
+	flag.Parse()
+
+	var handler http.Handler = http.DefaultServeMux
+	if *showDebug {
+		handler = debug.New(handler)
+	}
+
+	if *dir == "" {
+		d, err := os.Getwd()
+		if err != nil {
+			log.Fatal(err)
+		}
+		dir = &d
+	}
+
+	http.Handle("/", http.FileServer(http.Dir(*dir)))
+	log.Printf("Webserver starting on: http://%s", *listen)
+	log.Fatal(http.ListenAndServe(*listen, handler))
+}
